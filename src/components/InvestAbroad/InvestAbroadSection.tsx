6b503f9c35b4dd58dbf72be1785fa648316082fc
arsahak <arsahak99@gmail.com>
2024-07-07 18:58:26 +0600

    update invest aborad page

--- src/components/InvestAbroad/InvestAbroadSection.tsx
+++ src/components/InvestAbroad/InvestAbroadSection.tsx
@@ -1,39 +1,55 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 const InvestAbroadSection = () => {
+  const [selectTab, setSelectTab] = useState('E1 Investor Visa');
+
+  console.log('check select item 7', selectTab);
+
+  const tabData = [
+    {
+      id: 1,
+      title: 'E1 Investor Visa',
+      decs: "This setup leverages Next.js's image optimization features and ensures that the iframe and background image are displayed correctly across different screen sizes.",
+    },
+    {
+      id: 2,
+      title: 'E2 Investor Visa',
+      decs: "To achieve the desired styling where the bullet points are left-aligned and the text is right-aligned, you can use CSS Flexbox. Here's how you can adjust your list styling",
+    },
+    {
+      id: 3,
+      title: 'E3 Investor Visa',
+      decs: "To ensure that the height of the image and the content in your blog post remain the same, you can use flexbox and some Tailwind CSS utilities. Here's a refined version of your code",
+    },
+  ];
+
   return (
     <div className='mt-12'>
-      <div className='md:flex'>
-        <ul className='flex-column space-y space-y-4 text-sm font-medium text-gray-50 md:me-4 mb-4 md:mb-0'>
-          <li>
-            <a
-              href='#'
-              className='inline-flex items-center px-4 py-3 text-white bg-[#2F99CC] rounded-lg active w-full '
-              aria-current='page'
-            >
-              E1 Investor Visa
-            </a>
-          </li>
-          <li>
-            <a
-              href='#'
-              className='inline-flex items-center px-4 py-3 rounded-lg text-black hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full'
-            >
-              E2 Investor Visa
-            </a>
-          </li>
+      <div className='md:flex '>
+        <ul className='flex-2/4 flex-column space-y space-y-4 text-sm font-medium text-gray-50 md:me-4 mb-4 md:mb-0 bg-slate-100 p-10'>
+          {tabData?.map((item, index) => (
+            <li key={index} onClick={() => setSelectTab(item.title)}>
+              <a
+                href='#'
+                className={`inline-flex items-center px-4 py-3 text-black rounded-lg active w-full ${
+                  selectTab === item.title ? 'bg-[#2F99CC] text-white' : ''
+                }`}
+                aria-current='page'
+              >
+                {item.title}
+              </a>
+            </li>
+          ))}
         </ul>
-        <div className='p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full'>
-          <h3 className='text-lg font-bold text-gray-900 mb-2'>Profile Tab</h3>
-          <p className='mb-2'>
-            This is some placeholder content the Profile tab's associated
-            content, clicking another tab will toggle the visibility of this one
-            for the next.
-          </p>
-          <p>
-            The tab JavaScript swaps classes to control the content visibility
-            and styling.
-          </p>
+        <div className='flex-1/2 p-6 bg-gray-50 text-medium text-gray-500 rounded-lg w-full'>
+          {tabData
+            ?.filter((el, i) => el.title === selectTab)
+            .map((item, index) => (
+              <div key={index}>
+                <p className='text-xl'>{item?.decs}</p>
+              </div>
+            ))}
         </div>
       </div>
     </div>
